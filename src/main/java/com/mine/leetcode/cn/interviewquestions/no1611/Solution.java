9bdd300969f84acb30454dd7b72b236227721bc3
wangzhe18 <wangzhe18@lenovo.com>
2020-07-10 18:33:10 +0800

    [ADD]

---
+++ src/main/java/com/mine/leetcode/cn/interviewquestions/no1611/Solution.java
@@ -0,0 +1,18 @@
+package com.mine.leetcode.cn.interviewquestions.no1611;
+
+public class Solution {
+
+  public int[] divingBoard(int shorter, int longer, int k) {
+    if (k == 0) {
+      return new int[0];
+    }
+    if (shorter == longer) {
+      return new int[]{shorter * k};
+    }
+    int[] lengths = new int[k + 1];
+    for (int i = 0; i <= k; i++) {
+      lengths[i] = shorter * (k - i) + longer * i;
+    }
+    return lengths;
+  }
+}
